4b005fc3ca6dcdc1f159e57a09a99f5aa1e58170
Vadym Demchenko <dem27va@gmail.com>
2020-04-14 21:31:24 +0300

    img address update 4

--- modules/levels.js
+++ modules/levels.js
@@ -23,17 +23,17 @@ export const level1 = [
 
 export const level2 = [
     [0, 0, {n:1, i:'/img/2scope.png'}, 0, 0],
-    [{n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'img/block.png'}],
+    [{n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}],
     [{n:1, i:'/img/2css.png'}, 0, {n:1, i:'/img/2loop.png'}, 0, {n:1, i:'/img/2func.png'}],
-    [{n:1, i:'/img/block.png'}, {n:1, i:'/img/2arr.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/2rand.png'}, {n:1, i:'img/block.png'}]
+    [{n:1, i:'/img/block.png'}, {n:1, i:'/img/2arr.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/2rand.png'}, {n:1, i:'/img/block.png'}]
 ];
 
 export const level3 = [
     [0, 0, {n:1, i:'/img/3vue.png'}, 0, 0],
-    [{n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'img/block.png'}],
-    [{n:1, i:'/img/3obj.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/3class.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'img/3dom.png'}],
+    [{n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}],
+    [{n:1, i:'/img/3obj.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/3class.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/3dom.png'}],
     [{n:1, i:'/img/block.png'}, 0, {n:1, i:'/img/3oop.png'}, 0, {n:1, i:'/img/block.png'}],
-    [{n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'img/block.png'}]
+    [{n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}, {n:1, i:'/img/block.png'}]
 ];
 
 /*export const level4 = [
